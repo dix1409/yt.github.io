bb9352e3679311d03c99f63f889ee9c1b112fa12
dix1409 <83692300+dix1409@users.noreply.github.com>
2022-12-18 17:42:32 +0530

    Update index.js

--- index.js
+++ index.js
@@ -1,28 +1,23 @@
 const express = require("express");
 const app = express();
-const ytdl = require("ytdl-core");
+const ytdl = require("./ytdl-core/lib/index");
 
-app.set("view engine", "ejs");
+// app.set("view engine", "ejs");
 
-app.get("/", (req, res) => {
-	return res.render("index");
+app.get("/download", async (req, res) => {
+  const v_id = req.query.url.split("v=")[1];
+  const info = await ytdl.getInfo(req.query.url);
+  console.log(
+    info.formats.filter((format) => format.hasVideo && format.hasAudio)
+  );
+  //   console.log(info);
+
+  return res.json({
+    url: info.formats.filter((format) => format.hasVideo && format.hasAudio)[0]
+      .url,
+  });
 });
 
-app.get("/download", async(req, res) => {
-	const v_id = req.query.url.split('v=')[1];
-    const info = await ytdl.getInfo(req.query.url);
-    console.log(info.formats[4]);
-    console.log(info.formats[1]);
-
-	return res.render("download", {
-		url: "https://www.youtube.com/embed/" + v_id,
-        info: info.formats.sort((a, b) => {
-            return a.mimeType < b.mimeType;
-        }),
-	});
-});
-
-
-app.listen(3000, () => {
-	console.log("Server is running on http://localhost:3000");
+app.listen(3000||process.env.PORT, () => {
+  console.log("Server is running on http://localhost:3000");
 });
