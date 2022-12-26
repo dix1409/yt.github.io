9aa76c2d0eeff391dda30e32392a6391e06183cf
dix1409 <83692300+dix1409@users.noreply.github.com>
2022-12-26 11:02:15 +0530

    Update index.js

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.get("/download", async (req, res) => {
   );
 
 
-  return info.formats.filter((format) => format.hasVideo && format.hasAudio)[0];
+  return res.json({url:info.formats.filter((format) => format.hasVideo && format.hasAudio)[0]});
 });
 
 app.listen(3000||process.env.PORT, () => {
